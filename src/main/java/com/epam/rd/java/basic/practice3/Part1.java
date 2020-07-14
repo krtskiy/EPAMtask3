9a216d4058b8993debbd15afc306e8c00d039018
jMushmyH <jMushmyH@email.com>
2020-07-14 16:08:59 +0300

    4

--- src/main/java/com/epam/rd/java/basic/practice3/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part1.java
@@ -8,21 +8,26 @@ public class Part1 {
     public static void main(String[] args) {
         //just my empty main class
 
-        System.out.println(convert1(input));
+        System.out.print(convert1(input));
     }
 
     static String input = Util.getInput("part1.txt");
 
     public static String convert1(String input) {
 //        String regex1 = "\\s([a-z]+|[а-я]+)|(\\b([a-z]+)@(google|mail)\\.([a-z]+))";
-        String regex1 = ";[A-Z].+;|;[а-я].+;|[A-Z].+\\s";
+//        String regex1 = ";[A-Z].+;|;[а-я].+;|[A-Z].+\\s";
+        String regex1 = "^[A-Z][a-z]+;[A-Z][a-z]+;[A-Z][a-z]+\\s";  // убирает первую строку
         Pattern p1 = Pattern.compile(regex1);
         StringBuilder str = new StringBuilder();
         String[] test = p1.split(input);
         for (int i = 0; i < test.length; i++) {
             str.append(test[i]);
         }
-        return str.toString();
+        String str1 = str.toString();
+        String regex2 = ";[^;]*;";
+        Pattern p2 = Pattern.compile(regex2);
+        str1 = str1.replaceAll(regex2, ": ");
+        return str1;
     }
 
     public static String convert2(String input) {
