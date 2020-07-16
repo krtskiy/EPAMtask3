ed962861ed2206915113c7d6e30d9c3a95f5d062
jMushmyH <jMushmyH@email.com>
2020-07-16 21:17:55 +0300

    part3 test10

--- src/main/java/com/epam/rd/java/basic/practice3/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part3.java
@@ -7,7 +7,6 @@ public class Part3 {
 
     public static void main(String[] args) {
         //just my empty main class
-        System.out.println(convert(input));
     }
 
     static String regex = "([\\wА-Яа-я]{3,})";
@@ -43,11 +42,8 @@ public class Part3 {
             str.append(temp[i]).append(' ');
         }
         String result = str.toString();
-        result = result.replaceAll("\\s(?=\\s)", "");
         result = result.replaceAll("\\s$", "");
-//        result = result.replaceAll("\\s{2}", "\n" +
-//                "\n" +
-//                "    ");
+        result = result.replaceAll("\\s\\s", "\n");
         return result;
     }
 }
