ca314faea9227a42a6bc257fd80700945802aea9
jMushmyH <jMushmyH@email.com>
2020-07-17 00:10:11 +0300

    part2 superkostil' test

--- src/main/java/com/epam/rd/java/basic/practice3/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part2.java
@@ -7,24 +7,76 @@ public class Part2 {
 
     public static void main(String[] args) {
         //just my empty main class
+        System.out.println(convert(input));
     }
 
     static String input = Util.getInput("part2.txt");
     public static String convert(String input) {
-        String result;
+        int indexF = 0;
+        int indexS = 0;
+        String[] strArrF = new String[10];
+        String[] strArrF2 = new String[10];
+        String[] strArrS = new String[10];
+        String[] strArrS2 = new String[10];
         StringBuilder strMin = new StringBuilder("Min: ");
         StringBuilder strMax = new StringBuilder("Max: ");
-        Pattern pattern1 = Pattern.compile("\\b[A-Za-z]\\b");
-        Matcher matcher1 = pattern1.matcher(input);
-        while (matcher1.find()) {
-            strMin.append(input.substring(matcher1.start(), matcher1.end())).append(", ");
+        Pattern patternF = Pattern.compile("\\b[A-Za-z]\\b");
+        Matcher matcherF = patternF.matcher(input);
+        while (matcherF.find()) {
+            strArrF[indexF] = matcherF.group();
+            indexF++;
         }
-        Pattern pattern2 = Pattern.compile("\\b\\w{7}\\b");
-        Matcher matcher2 = pattern2.matcher(input);
-        while (matcher2.find()) {
-            strMax.append(input.substring(matcher2.start(), matcher2.end())).append(", ");
+        for (int i = 0; i < indexF; i++) {
+            if (i == 0) {
+            strMin.append(strArrF[0]).append(", ");
+            strArrF2[0] = strArrF[0];
+            } else {
+                for (int j = 0; j < strArrF2.length; ) {
+                    if (strArrF2[j].equals(strArrF[i])) {
+                        break;
+                    } else {
+                        if (i < indexF - 5) {
+                        strMin.append(strArrF[i]).append(", ");
+                        strArrF2[j + 1] = strArrF[i];
+                        j++;
+                        } else {
+                            strMin.append(strArrF[i]);
+                            strArrF2[j + 1] = strArrF[i];
+                            j++;
+                        }
+                    }
+                }
+            }
         }
-        result = strMin.append('\n').append(strMax).toString();
+        Pattern patternS = Pattern.compile("\\b[A-Za-z]{7}\\b");
+        Matcher matcherS = patternS.matcher(input);
+        while (matcherS.find()) {
+            strArrS[indexS] = matcherS.group();
+            indexS++;
+        }
+        for (int i = 0; i < indexS; i++) {
+            if (i == 0) {
+                strMax.append(strArrS[0]).append(", ");
+                strArrS2[0] = strArrS[0];
+            } else {
+                for (int j = 0; j < strArrS2.length; ) {
+                    if (strArrS2[j].equals(strArrS[i])) {
+                        break;
+                    } else {
+                        if (i < indexS - 1) {
+                        strMax.append(strArrS[i]).append(", ");
+                        strArrS2[j + 1] = strArrS[i];
+                        j++;
+                        } else {
+                            strMax.append(strArrS[i]);
+                            strArrS2[j + 1] = strArrS[i];
+                            j++;
+                        }
+                    }
+                }
+            }
+        }
+        String result = (strMin.append('\n').append(strMax)).toString();
         return result;
     }
 }
--- src/main/java/com/epam/rd/java/basic/practice3/Part6.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part6.java
@@ -1,13 +1,20 @@
 package com.epam.rd.java.basic.practice3;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Part6 {
 
     public static void main(String[] args) {
         //just my empty main class
+
     }
 
+    static String input = Util.getInput("part6.txt");
+
     public static String convert(String input) {
+
 
         return null;
     }
