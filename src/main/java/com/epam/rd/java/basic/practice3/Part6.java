69049609b1a772084272b867363ceb4c45eb9895
jMushmyH <jMushmyH@email.com>
2020-07-17 04:47:05 +0300

    part6 test govna 2

--- src/main/java/com/epam/rd/java/basic/practice3/Part6.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part6.java
@@ -1,35 +1,40 @@
 package com.epam.rd.java.basic.practice3;
 
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Part6 {
 
     public static void main(String[] args) {
         //just my empty main class
-        convert(input);
     }
 
     static String input = Util.getInput("part6.txt");
 
     public static String convert(String input) {
+        StringBuilder sb = new StringBuilder();
         String[] temp = input.split("\\b");
-        String[] temp2 = input.split("\\b");
         for (int i = 0; i < temp.length; i = i + 2) {
             int x = i + 2;
+            int y = i + 4;
             while (x < temp.length) {
                 if (temp[i].equals(temp[x])) {
+                    while (y < temp.length) {
+                        if (temp[i].equals(temp[y])) {
+                            temp[y] = "_" + temp[x];
+                        }
+                        y = y + 2;
+                    }
                     temp[i] = "_" + temp[i];
-                    temp[x] = "_" + temp[x];
                 }
                 x = x + 2;
             }
 
         }
-        for (String t : temp) {
-            System.out.print(t);
+        for (int i = 0; i < temp.length; i++) {
+            sb.append(temp[i]);
         }
-        return null;
+        String tempStr = sb.toString();
+        tempStr = tempStr.replaceAll("__", "_");
+        return tempStr;
     }
 }
