54078131d03fa1c1c789addc0463dd907544630f
jMushmyH <jMushmyH@email.com>
2020-07-18 18:16:26 +0300

    Part2 test1

--- src/main/java/com/epam/rd/java/basic/practice3/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part2.java
@@ -7,74 +7,82 @@ public class Part2 {
 
     public static void main(String[] args) {
         //just my empty main class
+        convert(input);
     }
 
     static String input = Util.getInput("part2.txt");
+
     public static String convert(String input) {
-        int indexF = 0;
-        int indexS = 0;
-        String[] strArrF = new String[10];
-        String[] strArrF2 = new String[10];
-        String[] strArrS = new String[10];
-        String[] strArrS2 = new String[10];
-        StringBuilder strMin = new StringBuilder("Min: ");
-        StringBuilder strMax = new StringBuilder("Max: ");
-        Pattern patternF = Pattern.compile("\\b[A-Za-z]\\b");
-        Matcher matcherF = patternF.matcher(input);
-        while (matcherF.find()) {
-            strArrF[indexF] = matcherF.group();
-            indexF++;
+        int indexLarge = 0;
+        int indexSmall = 0;
+        String[] arrLarge = new String[50];
+        String[] arrSmall = new String[50];
+        StringBuilder sbLarge = new StringBuilder("Min: ");
+        StringBuilder sbSmall = new StringBuilder("Max: ");
+        int size = 0;
+        String[] words = new String[input.length()];
+        String regex = "\\b\\w+?\\b";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(input);
+        while (m.find()) {
+            words[size] = m.group();
+            size++;
         }
-        for (int i = 0; i < indexF; i++) {
-            if (i == 0) {
-            strMin.append(strArrF[0]).append(", ");
-            strArrF2[0] = strArrF[0];
-            } else {
-                for (int j = 0; j < strArrF2.length; ) {
-                    if (strArrF2[j].equals(strArrF[i])) {
-                        break;
-                    } else {
-                        if (i < indexF - 5) {
-                        strMin.append(strArrF[i]).append(", ");
-                        strArrF2[j + 1] = strArrF[i];
-                        j++;
-                        } else {
-                            strMin.append(strArrF[i]);
-                            strArrF2[j + 1] = strArrF[i];
-                            j++;
-                        }
-                    }
-                }
+        String small = "";
+        String large = "";
+        int smallLength = 0;
+        int largeLength = 0;
+        small = large = words[0];
+        for (int i = 0; i < size; i++) {
+            if (small.length() > words[i].length()) {
+                small = words[i];
+                smallLength = small.length();
+            }
+            if (large.length() < words[i].length()) {
+                large = words[i];
+                largeLength = large.length();
             }
         }
-        Pattern patternS = Pattern.compile("\\b[A-Za-z]{7}\\b");
-        Matcher matcherS = patternS.matcher(input);
-        while (matcherS.find()) {
-            strArrS[indexS] = matcherS.group();
-            indexS++;
-        }
-        for (int i = 0; i < indexS; i++) {
-            if (i == 0) {
-                strMax.append(strArrS[0]).append(", ");
-                strArrS2[0] = strArrS[0];
+        String regexLarge = "\\b\\w{" + largeLength + "}?\\b";
+        String regexSmall = "\\b\\w{" + smallLength + "}?\\b";
+
+        indexLarge = findDuplicates(input, indexLarge, arrLarge, regexLarge);
+        indexSmall = findDuplicates(input, indexSmall, arrSmall, regexSmall);
+
+        for (int k = 0; k < indexLarge; k++) {
+            if (k < indexLarge - 1) {
+                sbLarge.append(arrLarge[k]).append(", ");
             } else {
-                for (int j = 0; j < strArrS2.length; ) {
-                    if (strArrS2[j].equals(strArrS[i])) {
-                        break;
-                    } else {
-                        if (i < indexS - 1) {
-                        strMax.append(strArrS[i]).append(", ");
-                        strArrS2[j + 1] = strArrS[i];
-                        j++;
-                        } else {
-                            strMax.append(strArrS[i]);
-                            strArrS2[j + 1] = strArrS[i];
-                            j++;
-                        }
-                    }
-                }
+                sbLarge.append(arrLarge[k]);
             }
         }
-        return strMin.append('\n').append(strMax).toString();
+        for (int l = 0; l < indexSmall; l++) {
+            if (l < indexSmall - 1) {
+                sbSmall.append(arrSmall[l]).append(", ");
+            } else {
+                sbSmall.append(arrSmall[l]);
+            }
+        }
+
+        return sbSmall.append("\n").append(sbLarge).toString();
+    }
+
+    private static int findDuplicates(String input, int indexLarge, String[] arrLarge, String regexLarge) {
+        Pattern pLarge = Pattern.compile(regexLarge);
+        Matcher mLarge = pLarge.matcher(input);
+        while (mLarge.find()) {
+            if (indexLarge == 0) {
+                arrLarge[indexLarge] = mLarge.group();
+                indexLarge++;
+            }
+            for (int i = 0; i < indexLarge; i++) {
+                if (!arrLarge[i].equals(mLarge.group())) {
+                    arrLarge[indexLarge] = mLarge.group();
+                    indexLarge++;
+                }
+                break;
+            }
+        }
+        return indexLarge;
     }
 }
